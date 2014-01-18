46ffad982512c9a6bacc7542924391b1e390216f
theowner997 <georgelaf997@hotmail.com>
2014-01-18 18:33:42 +0200

    Update unlock.bat

--- Lg-l7/bootloader/unlock.bat
+++ Lg-l7/bootloader/unlock.bat
@@ -1,2 +1,7 @@
 @echo off
 cls
+@SET ADB=c:\l7cwm\adb
+%ADB% wait-for-device
+%ADB% push emmc_appsboot.bin /data/local/tmp/emmc_appsboot.bin
+%ADB% shell su -c "dd if=/data/local/tmp/emmc_appsboot.bin of=/dev/block/mmcblk0p5 bs=4096"
+%ADB% reboot
