f6c1e46eafb91ec94f9d8e142734ac44596d4fe5
Joey Rizzoli <io.nolinuxnoparty@gmail.com>
2014-01-18 17:02:36 +0100

    Update Start.bat
    added splash screen

--- common/Start.bat
+++ common/Start.bat
@@ -1,2 +1,49 @@
 @echo off
 cls
+color F2
+mode con cols=81
+mode con lines=45
+echo.
+echo    ###########################################################################
+echo    ##                                                                       ## 
+echo    ##                         iN                iZ                          ## 
+echo    ##                          Uo               B                           ## 
+echo    ##                           vLioOBBBBBBMqviq                            ##
+echo    ##                          vMBBBBBBBBBBBBBBBVi                          ## 
+echo    ##                        FBBBBBMOOOOOOOOMBBBBBMi                        ## 
+echo    ##                      iBBBB  iBOGGGGGGGBP  oBBBI                       ## 
+echo    ##                     iBBMMM  iBOGGGGGGGBk  uBMBBN                      ##
+echo    ##                     BBMOOBBBBMOOOOOOOOMBBBBMOMBBv                     ##
+echo    ##                    IBBBBBBBBBBBBBBBBBBBBBBBBBBBBB                     ## 
+echo    ##                    YNUAAAAAAAAAAAAAAAAAAAAAAAAAVG                     ##
+echo    ##              VZPi                                   uZqr              ## 
+echo    ##            iBBBBBF qBBBBBBBBBBBBBBBBBBBBBBBBBBBBB  BBBBBM             ## 
+echo    ##            XBMOMBM uBMMMMMMMMMMMMMMMMMMMMMMMMMMBB iBBOMBB             ## 
+echo    ##            UBOGOBZ JBOGGGGGGGGGGGGGGGGGGGGGGGGOMB iBMGOBB             ## 
+echo    ##            uBOGOBE JBOGGGGGGGGGGGGGGGGGGGGGGGGGMB iBMGOBB             ## 
+echo    ##            UBOGOBE JBOGGGGGGGGGGGGGGGGGGGGGGGGOMB iBMGOBB             ## 
+echo    ##            UBOGOBE JBOGGGGGGGGGGGGGGGGGGGGGGGGOMB iBMGOBB             ## 
+echo    ##            uBOGOBE JBOGGGGGGGGGGGGGGGGGGGGGGGGOMB iBMGOBB             ## 
+echo    ##            UBOGOBE JBOGGGGGGGGGGGGGGGGGGGGGGGGOMB iBMGOBB             ## 
+echo    ##            FBMOOBG JBOGGGGGGGGGGGGGGGGGGGGGGGGOMB iBMOOBB             ## 
+echo    ##            uBBBBBM uBOGGGGGGGGGGGGGGGGGGGGGGGGGMB iBBBBBB             ## 
+echo    ##             FBBBM  VBOGGGGGGGGGGGGGGGGGGGGGGGGGMB  jBBBBi             ## 
+echo    ##                    PBMGGGGGGGGGGGGGGGGGGGGGGGGOBB                     ## 
+echo    ##                    XBBBMMOOGGGGOMMMMOOGGGGOMMMBBB                     ##  
+echo    ##                     PBBBBBMOGGOBBBBBBMOGGOMBBBBBi                     ## 
+echo    ##                          uBOGOBG    rBMGGMB                           ## 
+echo    ##                          vBOGOBG    iBMGGBB                           ## 
+echo    ##                          uBOGOBM    iBMGGBB                           ## 
+echo    ##                          oBMGOBB    iBMGOBB                           ## 
+echo    ##                          XBBMBBB    rBBMMBB                           ## 
+echo    ##                           BBBBBi     MBBBBj                           ## 
+echo    ##                             ii         ii                             ## 
+echo    ##                                                                       ## 
+echo    ##                                                                       ##
+echo    ##                Lg L Toolkit    by linuxx & theOwner977                ##
+echo    ##                                                                       ##
+echo    ###########################################################################
+ECHO.
+pause
+color 2
+goto :start
