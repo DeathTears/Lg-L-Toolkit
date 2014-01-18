19efad36c4a499ca15ca5b08d1d7aca6bf5e143d
Joey Rizzoli <io.nolinuxnoparty@gmail.com>
2014-01-18 17:17:45 +0100

    Update Start.bat
    start adding startui infos

--- common/Start.bat
+++ common/Start.bat
@@ -46,4 +46,64 @@ echo    ########################################################################
 ECHO.
 pause
 color 2
-goto :start
+goto :look
+
+
+:startui
+echo ##############################
+echo Device: $device   Android Api:
+echo Status: $status
+echo
+
+
+
+
+
+
+
+if [ ! -e tools/deodex_files/api_level.txt ]
+then
+  api_level=UNKNOWN
+else
+
+  # Fix Linux issue with carriage returns
+  sed -i -e '/^$/d' tools/deodex_files/api_level.txt
+  api_level=`more tools/deodex_files/api_level.txt | sed 's/[ ]*$//g'`
+  if [ "$api_level" == "" ]
+  then
+    api_level=UNKNOWN
+  fi
+fi  
+
+echo $api_level
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+:look
+echo Plug your phone in
+#see what device is currently plugged in
+#if it's e400 = l3
+#if it's e610 = l5
+#if it's p700 = l7
+
+if %S%==1 goto menu1
+if %S%==2 goto sync
+if %S%==3 goto testnm
+if %S%==5 goto advanced
+if %S%==4 goto backupc
+if %S%==0 goto exit
+
+@
