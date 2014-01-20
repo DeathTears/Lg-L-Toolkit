2a4fdca772feae18280a304f5dbdb400a09393a5
Joey Rizzoli <io.nolinuxnoparty@gmail.com>
2014-01-20 21:23:29 +0100

    Update Setup.bat
    Removed var.

--- Setup/Setup.bat
+++ Setup/Setup.bat
@@ -1,31 +1,31 @@
 @echo off
 cls
-mode con cols=56
-mode con lines=27
-echo |||
-echo |||
-echo |||
-echo |||
-echo |||
-echo |||
-echo |||
-echo |||
-echo |||
-echo |||
-echo |||
-echo |||
-echo |||                             ToolKit ---- Setup
-echo |||
-echo |||                      By Linuxx
-echo |||                              and
-echo |||                           theOwner997
-echo |||
-echo |||
-echo |||
-echo |||
-echo |||_____________________________|
-echo |||_____________________________|
-echo |_______________________________|
+
+
+echo MBBG
+echo MBBG
+echo MBBG
+echo MBBG
+echo MBBG
+echo MBBG
+echo MBBG
+echo MBBG
+echo MBBG
+echo MBBG
+echo MBBG
+echo MBBG
+echo MBBG     ToolKit                ----       Setup
+echo MBBG
+echo MBBG                     By Linuxx
+echo MBBG                              and
+echo MBBG                          theOwner997
+echo MBBG
+echo MBBG
+echo MBBG
+echo MBBG
+echo MBBGGGGGGGGGGGGGHHHHHIIIIIIIIIIIII
+echo MBBBBBBBBBBBBBBBBBBBBBBGGGGGGGGHHI
+echo MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMI
 echo.
 echo.
 pause
@@ -34,21 +34,15 @@ echo Setting up PC...
 @erase C:\LgTool\*
 @erase C:\LgTool
 echo Copying files...
-@set $adb=Drivers\
-@set $l3=Lg-l3
-@set $l5=Lg-l5
-@set $l7=Lg-l7
-@set $com=common 
-@set $ll=C:\LgTool\
 echo.
 @md C:\LgTool
-copy %adb% C:\LgTool\
-copy %l3% C:\LgTool\
-copy %l5% C:\LgTool\
-copy %l7% C:\LgTool\
-mkdir C:\LgTool\common
-copy %com%\Root %ll%\common\
-copy %com%\Backup %ll%\common\
-copy Start.exe %ll%
-echo Done!
-%ll%\Start.exe
+@copy Drivers\* C:\LgTool\
+@copy Lg-l3 C:\LgTool\
+@copy Lg-l5 C:\LgTool\
+@copy Lg-l7 C:\LgTool\
+@mkdir C:\LgTool\common
+@copy common\RootC:\LgTool\common\
+@copy common\Backup C:\LgTool\common\
+@copy Start.exe C:\LgTool\
+@echo Done!
+C:\LgTool\Start.exe
