359e6ba00a86d4062eb76d2df834d6172e1c2d9d
Joey Rizzoli <io.nolinuxnoparty@gmail.com>
2014-01-20 14:15:27 +0100

    Update unlock.bat
    Sholud Work Now

--- Lg-l7/bootloader/unlock.bat
+++ Lg-l7/bootloader/unlock.bat
@@ -1,3 +1,34 @@
 @echo off
 cls
-push emmc_appsboot.bin /data/local/tmp/emmc_appsboot.bin
+echo ###################################################################
+echo -->Device:
+adb shell grep ro.product.model= system/build.prop
+echo -->Codename
+adb shell grep ro.product.device= system/build.prop
+echo -->Android version:
+adb shell grep ro.build.version.release= system/build.prop
+echo -->Api:
+adb shell grep ro.build.version.sdk= system/build.prop
+echo Status:
+adb get-state
+echo Serial Number:
+adb get-serialno
+echo ####################################################################
+echo.
+echo.
+echo Unlocking Bootloder....
+@adb root
+echo Looking for device....
+adb wait-for-device
+echo Found a Device, if it's a Lg L7,
+pause
+adb remount
+echo Pushing Bootloader.....
+@adb push emmc_appsboot.bin /sddcard/
+echo Writing.....
+@adb shell su -c "dd if=/data/local/tmp/emmc_appsboot.bin of=/dev/block/mmcblk0p5 bs=4096"
+@adb reboot
+@adb wait-for device
+@adb shell rm /sdcard/emmc_appsboot.bin
+echo Done!
+C:\LgTool\common\Start.bat
