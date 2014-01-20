0ce3ff08839aa6ca3ec58ef72be589a62e1c9ad9
Joey Rizzoli <io.nolinuxnoparty@gmail.com>
2014-01-20 14:10:24 +0100

    Update unlock.bat

--- Lg-l5/Bootloader/unlock.bat
+++ Lg-l5/Bootloader/unlock.bat
@@ -1,3 +1,8 @@
 @echo off
-adb push emmc_appsboot.bin ecc
+cls
+echo 
+@adb root
+adb wait-for-device
+adb remount
+adb push emmc_appsboot.bin /sddcard/
 dd if=/sdcard/emmc_appsboot.bin of=/dev/block/mmcblk0p5
